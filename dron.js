54cd67f12843a61937e967ae88247a241d310fc4
Łukasz <163403594+Syntaxer64@users.noreply.github.com>
2024-04-17 21:36:00 +0200

    Update dron.js
    Dodanie komentarzy

--- dron.js
+++ dron.js
@@ -1,44 +1,41 @@
 class Dron {
-    r = 10;
-    pos = createVector(width / 2, height / 2);
-    vel = createVector(0, 0.5);
-    acc = createVector(0, 0.15);
-    opor = createVector(0, 1.5);
-    drone_size = 100;
-    flyKeyPressed=false;
+    r = 10; //promień okręgu (drona)
+    pos = createVector(width / 2, height / 2); //pozycja drona na płótnie (canvas)
+    vel = createVector(0, 0.5); //prędkość grawitacji drona
+    acc = createVector(0, 0.15); //przyspieszenie grawitacyjne drona
+    opor = createVector(0, 1.5); //opór drona
+    drone_size = 100; //wielkość drona
+    flyKeyPressed=false; //sprawdza, czy przycisk jest naciśnięty (default=fałsz)
   
     update = function () {
-      this.pos.add(this.vel);
-      this.vel.add(this.acc);
-      if (this.pos.y > height - this.r / 2) {
+      this.pos.add(this.vel); //dodanie prędkości do pozycji
+      this.vel.add(this.acc); //dodanie przyspieszenia do pozycji
+      if (this.pos.y > height - this.r / 2) { //warunek, który sprawdza czy dron nie dotyka podłogi
         this.vel.y = 0;
         this.acc.y = 0;
         this.pos.y = height - this.r / 2;
       }
-      if (this.pos.x >= width) {
-        this.pos.x = 0;
-      }
-      this.acc.add(this.opor);
-      if (this.acc.y > 0.5) this.acc.y = 0.3;
-      this.vel.limit(8);
+      this.acc.add(this.opor); //dodanie oporu do przyspieszenia
+      if (this.acc.y > 0.5) this.acc.y = 0.3; //nadanie limitu przyspieszeniu
+      this.vel.limit(8); //nadanie limitu prędkości
 
-      if (this.flyKeyPressed) {
+      if (this.flyKeyPressed) { //jeżeli flyKeyPressed jest true, to dodaj przyspieszenie ujemne do drona
         var fly_acc = createVector(0, -0.5);
         this.acc.add(fly_acc);
     };
 };
   
-    draw = function () {
+    draw = function () { //rysowanie drona
       stroke(0, 0);
       fill(113, 215, 217);
       ellipse(this.pos.x, this.pos.y, this.r);
     };
   
-    fly = function () {
+    fly = function () { //funkcja sprawdzająca, czy dron ma lecieć do góry
       this.flyKeyPressed=true;
     };
 
-    fall = function () {
+    fall = function () { //funkcja sprawdzająca, czy dron ma spadać
         this.flyKeyPressed=false;
     }
   };
