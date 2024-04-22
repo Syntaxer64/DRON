e438e61ee0827b7ccc4f3a60fcb42691cba69dcb
Łukasz <163403594+Syntaxer64@users.noreply.github.com>
2024-04-22 20:09:50 +0200

    Update tlo.js
    Dodanie komentarzy

--- tlo.js
+++ tlo.js
@@ -1,19 +1,19 @@
 class Tlo {
     constructor(image) {
-      this.image = image;
-      this.speed = 2;
-      this.x = 0;
+      this.image = image; //zmienna ze ścieżką do pliku tła
+      this.speed = 2; //prędkość przewijania się tła
+      this.x = 0; //początkowa pozycja lewego boku tła
     }
   
     update() {
-      this.x -= this.speed;
-      if (this.x <= -width) {
-        this.x = 0;
+      this.x -= this.speed; //z każdą klatką zmień pozycję tła w lewo o zadaną prędkość
+      if (this.x <= -width) { //jeżeli tło wyjdzie w całości poza ekran
+        this.x = 0; //ustaw pozycję lewego boku tła znowu na 0
       }
     }
   
     draw() {
       image(this.image, this.x, 0, width, height);
-      image(this.image, this.x + width, 0, width, height);
+      image(this.image, this.x + width, 0, width, height); //wygenerowanie dwóch powtarzających się po sobie obrazów jako tło gry
     }
   }
