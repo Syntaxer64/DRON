964d5993d06930847e9dad8ac7b3fc1ab9e07ba9
KubaCholewa <167408838+KubaCholewa@users.noreply.github.com>
2024-04-24 21:25:20 +0200

    Update koniecGry.js

--- koniecGry.js
+++ koniecGry.js
@@ -1,31 +1,32 @@
-class KoniecGry {
-    constructor(endTime) {
-        this.endTime = endTime; //zmienna z czasem rozbicia się
-      }
-    
-      draw() {
-        fill(0, 150); 
-        rect(0, 0, width, height); //przyciemnij grę znajdującą się za oknem końca gry
-    
-        fill(255);
-        rect(width / 4, height / 4, width / 2, height / 2); //narysuj okno końca gry
-    
-        let timeElapsed = this.endTime - startTime; //czas, jaki minął od startu gry
-        let minutes = Math.floor(timeElapsed / (1000 * 60)); //ile minut minęło od startu
-        let seconds = Math.floor((timeElapsed % (1000 * 60)) / 1000); //ile sekund minęło od startu
-        let milliseconds = nf(timeElapsed % 1000, 3).slice(0, 3); // Pokazuje tylko pierwsze trzy cyfry milisekund
+class Przeszkoda {
+  constructor(obs_x, obs_height, isTop) {
+    this.obs_width = 90; //szerokość przeszkody
+    this.obs_x = obs_x; //wspołrzędna lewego górnego wierzchołka przeszkody
+    this.vel = createVector(-6, 0); //prędkość przeszkody
+    this.obs_height = obs_height; //wysokość przeszkody
+    this.isTop = isTop; //sprawdza, czy przeszkoda powinna znajdować się na górze
+  }
 
-    
-        fill(0); 
-        textSize(24);
-        textAlign(CENTER, CENTER);
-        text("ROZBIŁEŚ SIĘ :(", width / 2, height / 2 - 80);
-        text("Twój czas: " + 
-          nf(minutes, 2) + ":" + 
-          nf(seconds, 2) + "," + 
-          nf(milliseconds, 3), width / 2, height / 2 + 20); //tekst z czasem końcowym
-    
-        textSize(16);
-        text("Naciśnij przycisk, by spróbować ponownie.", width / 2, height * 2/3); //informacja o restarcie gry
-      }
+  update = function () {
+    this.obs_x += this.vel.x; //dodanie prędkości do pozycji przeszkody
+  };
+
+  draw = function () { //rysowanie przeszkody
+    stroke(0, 0);
+    fill(113, 215, 217);
+    if (this.isTop) { //jeżeli isTop jest true, to przeszkoda jest na górze
+      rect(this.obs_x, 0, this.obs_width, this.obs_height);
+    } else { //jeżeli nie, to na dole
+      rect(
+        this.obs_x,
+        height - this.obs_height,
+        this.obs_width,
+        this.obs_height
+      );
     }
+  };
+
+  offscreen = function () { //funkcja sprawdzająca, czy przeszkoda wyszła poza ekran
+    return this.obs_x < -this.obs_width;
+  };
+}
