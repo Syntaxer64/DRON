17e378afd39a1a386592bcefe0f9672912d5d715
KubaCholewa <167408838+KubaCholewa@users.noreply.github.com>
2024-04-24 21:26:08 +0200

    Update przeszkoda.js

--- przeszkoda.js
+++ przeszkoda.js
@@ -1,8 +1,8 @@
 class Przeszkoda {
   constructor(obs_x, obs_height, isTop) {
-    this.obs_width = 150; //szerokość przeszkody
+    this.obs_width = 90; //szerokość przeszkody
     this.obs_x = obs_x; //wspołrzędna lewego górnego wierzchołka przeszkody
-    this.vel = createVector(-4, 0); //prędkość przeszkody
+    this.vel = createVector(-6, 0); //prędkość przeszkody
     this.obs_height = obs_height; //wysokość przeszkody
     this.isTop = isTop; //sprawdza, czy przeszkoda powinna znajdować się na górze
   }
